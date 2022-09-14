8283d09be65a8529f784c24fd76a351355e05a30
a9500484054 <a9500484054@yandex.ru>
2022-09-14 03:23:02 +0300

    first commit

--- js/app.js
+++ js/app.js
@@ -1,48 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const menuBtn = document.querySelector('.menu-btn')
+    const menuClose = document.querySelector('.menu__close')
+    const searchBtn = document.querySelector('.search-btn')
+    const searchClose = document.querySelector('.search__close')
     const menu = document.querySelector('.menu')
-
+    const search = document.querySelector('.search')
+    const bodyElement = document.body
 
     menuBtn.addEventListener('click', () => {
-        menuBtn.classList.toggle('menu-btn--active')
-        menu.classList.toggle('menu--active')
+        menu.classList.add('menu--active')
+        bodyElement.style.overflow = "hidden"
+    })
+
+    menuClose.addEventListener('click', () => {
+        menu.classList.remove('menu--active')
+        bodyElement.style.overflow = "auto"
+    })
+
+    searchBtn.addEventListener('click', () => {
+        search.classList.add('search--active')
+        bodyElement.style.overflow = "hidden"
+    })
+
+    searchClose.addEventListener('click', () => {
+        search.classList.remove('search--active')
+        bodyElement.style.overflow = "auto"
     })
 
 
-    const whom = document.querySelector('.whom');
-    const blockTwoBtn = document.querySelectorAll('.block-two__btn');
-    const blockThree = document.querySelectorAll('.block-three');
-
-    if (whom) {
-        whom.addEventListener('click', (e) => {
-            if (e.target.classList.contains('block-two__btn')) {
-                const tabsPath = e.target.dataset.tabsPath;
-                blockTwoBtn.forEach(el => {el.classList.remove('block-two__btn--active')});
-                document.querySelector(`[data-tabs-path="${tabsPath}"]`).classList.add('block-two__btn--active');
-                umInvestorLpFeaturesHandler(tabsPath);
-            }
-        });
-    }
-    
-    const umInvestorLpFeaturesHandler = (path) => {
-        blockThree.forEach(el => {el.classList.remove('block-three--active')});
-        document.querySelector(`[data-tabs-target="${path}"]`).classList.add('block-three--active');
-    };
-
-    const blockThreeButton = document.querySelectorAll('.block-three__button');
-
-    blockThreeButton.forEach(el => {
-        el.addEventListener('click', () => {
-            blockThree.forEach(el => {el.classList.remove('block-three--active')});
-        })
-    })
-
-});
-
-$('a[href^="#"]').click(function(){ // #1
-    let anchor = $(this).attr('href');  // #2
-    $('html, body').animate({           // #3
-    scrollTop:  $(anchor).offset().top  // #4
-    }, 600);                            // #5
 });
