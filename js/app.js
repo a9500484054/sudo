925d7148c187178660b539857e817f64bc8f24a5
a9500484054 <a9500484054@yandex.ru>
2022-09-17 20:53:59 +0300

    Внес исправления

--- js/app.js
+++ js/app.js
@@ -3,30 +3,27 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuBtn = document.querySelector('.menu-btn')
     const menuClose = document.querySelector('.menu__close')
     const searchBtn = document.querySelector('.search-btn')
-    const searchClose = document.querySelector('.search__close')
     const menu = document.querySelector('.menu')
     const search = document.querySelector('.search')
     const bodyElement = document.body
 
     menuBtn.addEventListener('click', () => {
-        menu.classList.add('menu--active')
-        bodyElement.style.overflow = "hidden"
+        menu.classList.toggle('menu--active')
+        menuBtn.classList.toggle('menu-btn--active')
+        bodyElement.classList.toggle('overflow')
+        
+
     })
 
-    menuClose.addEventListener('click', () => {
-        menu.classList.remove('menu--active')
-        bodyElement.style.overflow = "auto"
+
+    const menuLinkLlist = document.querySelector('.menu__link-list')
+    const menuList2 = document.querySelector('.menu__list-2')
+
+    menuLinkLlist.addEventListener('click', () => {
+        menuList2.classList.toggle('menu__list-2--active')
     })
 
-    searchBtn.addEventListener('click', () => {
-        search.classList.add('search--active')
-        bodyElement.style.overflow = "hidden"
-    })
-
-    searchClose.addEventListener('click', () => {
-        search.classList.remove('search--active')
-        bodyElement.style.overflow = "auto"
-    })
+    
 
     const servicesBlockNavBtnLeft = document.querySelector('.services-block__nav-btn-l')
     const servicesBlockNavBtnRight = document.querySelector('.services-block__nav-btn-r')
