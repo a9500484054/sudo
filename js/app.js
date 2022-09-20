bbcf16c5ae4a1b5a578a2a426c04ff1de59d9d17
a9500484054 <a9500484054@yandex.ru>
2022-09-21 00:09:20 +0300

    Внес исправления

--- js/app.js
+++ js/app.js
@@ -28,60 +28,60 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
     
-    // const indexBlockBbtn = document.querySelectorAll('.index-block__btn')
-    // const modalFeedback = document.querySelector('.modal-feedback')
-    // const modalFeedbackClose = document.querySelector('.modal-feedback__close')
+    const indexBlockBbtn = document.querySelectorAll('.index-block__btn')
+    const modalFeedback = document.querySelector('.modal-feedback')
+    const modalFeedbackClose = document.querySelector('.modal-feedback__close')
 
-    // indexBlockBbtn.forEach(function(event) {
-    //     event.addEventListener('click', function(event) {
-    //         modalFeedback.classList.add('modal-feedback--active')
+    indexBlockBbtn.forEach(function(event) {
+        event.addEventListener('click', function(event) {
+            modalFeedback.classList.add('modal-feedback--active')
 
-    //         modalFeedback.scrollIntoView({
-    //             behavior: 'smooth',
-    //             block: 'start'
-    //         })
-    //     })
-    // })
+            modalFeedback.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+        })
+    })
 
-    // modalFeedbackClose.addEventListener('click', () => {
-    //     modalFeedback.classList.remove('modal-feedback--active')
-    // })
+    modalFeedbackClose.addEventListener('click', () => {
+        modalFeedback.classList.remove('modal-feedback--active')
+    })
 
-    // const servicesBlockNavBtnLeft = document.querySelector('.services-block__nav-btn-l')
-    // const servicesBlockNavBtnRight = document.querySelector('.services-block__nav-btn-r')
-    // const servicesBlockItem = document.querySelectorAll('.services-block__item')
-    // const servicesBlockProgess = document.querySelector('.services-block__progess--elem')
-    // let count = 0
-    // let positionElement = 0
+    const servicesBlockNavBtnLeft = document.querySelector('.services-block__nav-btn-l')
+    const servicesBlockNavBtnRight = document.querySelector('.services-block__nav-btn-r')
+    const servicesBlockItem = document.querySelectorAll('.services-block__item')
+    const servicesBlockProgess = document.querySelector('.services-block__progess--elem')
+    let count = 0
+    let positionElement = 0
 
-    // let i = 100/servicesBlockItem.length
-    // console.log(i)
+    let i = 100/servicesBlockItem.length
+    console.log(i)
 
-    // servicesBlockProgess.style.width = i + '%'
+    servicesBlockProgess.style.width = i + '%'
     
-    // servicesBlockNavBtnRight.addEventListener('click', () => {
-    //     count++
-    //     positionElement += i
-    //     servicesBlockNavBtnLeft.removeAttribute('disabled');
-    //     if(count == servicesBlockItem.length - 1) {
-    //         servicesBlockNavBtnRight.setAttribute('disabled', false);
-    //     }
-    //     servicesBlockProgess.style.left = positionElement + "%"
-    //     servicesBlockItem.forEach(el => el.classList.remove('services-block__item--active'))
-    //     servicesBlockItem[count].classList.add('services-block__item--active')
-    // })
+    servicesBlockNavBtnRight.addEventListener('click', () => {
+        count++
+        positionElement += i
+        servicesBlockNavBtnLeft.removeAttribute('disabled');
+        if(count == servicesBlockItem.length - 1) {
+            servicesBlockNavBtnRight.setAttribute('disabled', false);
+        }
+        servicesBlockProgess.style.left = positionElement + "%"
+        servicesBlockItem.forEach(el => el.classList.remove('services-block__item--active'))
+        servicesBlockItem[count].classList.add('services-block__item--active')
+    })
 
-    // servicesBlockNavBtnLeft.addEventListener('click', () => {
-    //     count--
-    //     positionElement -= i
-    //     servicesBlockNavBtnRight.removeAttribute('disabled');
-    //     if(count == 0) {
-    //         servicesBlockNavBtnLeft.setAttribute('disabled', true);
-    //     }
-    //     servicesBlockProgess.style.left = positionElement + "%"
-    //     servicesBlockItem.forEach(el => el.classList.remove('services-block__item--active'))
-    //     servicesBlockItem[count].classList.add('services-block__item--active')
-    // })
+    servicesBlockNavBtnLeft.addEventListener('click', () => {
+        count--
+        positionElement -= i
+        servicesBlockNavBtnRight.removeAttribute('disabled');
+        if(count == 0) {
+            servicesBlockNavBtnLeft.setAttribute('disabled', true);
+        }
+        servicesBlockProgess.style.left = positionElement + "%"
+        servicesBlockItem.forEach(el => el.classList.remove('services-block__item--active'))
+        servicesBlockItem[count].classList.add('services-block__item--active')
+    })
 
 
     const ProjectsSortBtn = document.querySelectorAll('.projects__sort-btn')
